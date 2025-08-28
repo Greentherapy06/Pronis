e8d1b6e85a7aab9e6ceebd48bebdf85c3357904d
Gladiator 🇮🇹 <greentherapy06@gmail.com>
2025-08-28 03:56:19 +0100

    Update methods.js

--- api/methods.js
+++ api/methods.js
@@ -1,10 +1,5 @@
-// api/methods.js
 export default function handler(req, res) {
   res.status(200).json([
-    {
-      id: "crypto",
-      name: "Payer en crypto (NOWPayments)",
-      checkoutUrl: "https://green-therapy-cbd.vercel.app/api/checkout"
-    }
+    { id: 'crypto', name: 'Payer en crypto (NOWPayments)', checkoutUrl: 'https://green-therapy-cbd.vercel.app/api/checkout' }
   ]);
 }
